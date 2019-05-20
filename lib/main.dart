c6c37658936eaa2a0a64046fe8720bdc00e8300f
dan-rds <ddrichar@ucsc.edu>
2019-05-20 16:48:25 -0700

    adding sys call

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'dart:core';
 
+import 'dart:io';
+
 clog() {
   var s = "___string instance";
   print(s);
   print(s.runtimeType);
+  Process.run('ls', ['-l']).then((ProcessResult results) {
+    print(results.stdout);
+  });
 }
