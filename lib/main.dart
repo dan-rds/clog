38f837a28771a786e65bb027d113fe8a8a771330
dan-rds <ddrichar@ucsc.edu>
2019-05-20 17:41:06 -0700

    all colors printing func

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,33 @@
 import 'dart:core';
+import 'dart:collection';
 
 import 'dart:io';
 
+String _start = "\x1B[";
+
+String black_code = "30";
+String red_code = "31";
+String green_code = "32";
+String yellow_code = "33";
+String blue_code = "34";
+String magenta_code = "35";
+String cyan_code = "36";
+String white_code = "37";
+String reset_code = "0";
+String _end = "m";
+
+String esc_sequence(color) => _start + color + "m";
+
+String build_string(s, color_code) => esc_sequence(color_code) + s + esc_sequence('0');
+
 clog() {
-  var s = "___string instance";
-  print(s);
-  print(s.runtimeType);
-  Process.run('ls', ['-l']).then((ProcessResult results) {
-    print(results.stdout);
-  });
-  print("Tried to run ^^");
+  List log_types = ["ERROR", "DEBUG", "WARNING", "INFO"];
+  List color_log_type = [red_code, blue_code, yellow_code, green_code];
+
+  void all() {
+    for (var i = 0; i < log_types.length; i++) {
+      //print(log_types[i] + color_log_type[1]);
+      print(build_string(log_types[i], color_log_type[i]));
+    }
+  }
 }
